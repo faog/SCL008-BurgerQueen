c749214169ae42bd84c636ebdaa11aa05f8749db
faog <fabiola.orellana.g@gmail.com>
2019-05-10 15:04:08 -0400

    Creación de componentVisualContainerWaiter, junto a estructura de prueba

---
+++ src/componentsVisual/componentVisualContainerWaiter.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class ComponentVisualContainerWaiter extends Component{
+    render(){
+        return(          
+            <h1>Hola Mesero</h1>           
+        )
+    }
+}
+export default ComponentVisualContainerWaiter;
