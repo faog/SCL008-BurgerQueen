1f1774140d7620da200e20275eba7a246bec9ce3
faog <fabiola.orellana.g@gmail.com>
2019-05-10 15:08:21 -0400

    Se cambia función por clase, creando constructor y uso de bind(this)

--- src/componentsVisual/componentVisualRoleButton.js
+++ src/componentsVisual/componentVisualRoleButton.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
+class ComponentVisualRoleButton extends Component{
+    constructor (props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(){
+        this.props.buttonOnClick();
+    }
 
-const ComponentVisualRoleButton = (props) => <button>{props.role}</button>           
+    render(){
+        return(
+            <button type="button" onClick = {this.handleClick}>{this.props.roles}</button> 
+        )
+    }
+}    
 
 export default ComponentVisualRoleButton;
