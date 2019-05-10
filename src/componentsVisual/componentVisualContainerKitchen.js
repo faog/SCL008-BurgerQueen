a9b3fb1d9c15d185c5e07b1b124fca17ba6b4c9e
faog <fabiola.orellana.g@gmail.com>
2019-05-10 15:04:34 -0400

    Creación de componentVisualContainerKitchen, junto a estructura de prueba

---
+++ src/componentsVisual/componentVisualContainerKitchen.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class ComponentVisualContainerKitchen extends Component{
+    render(){
+        return(          
+            <h1>Hola Cocinero</h1>           
+        )
+    }
+}
+export default ComponentVisualContainerKitchen;
