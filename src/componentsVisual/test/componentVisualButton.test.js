f4c21b17dc66b86f50c89f56d435041aa9453736
faog <fabiola.orellana.g@gmail.com>
2019-05-13 14:44:03 -0400

    Test de prueba para el componente boton

---
+++ src/componentsVisual/test/componentVisualButton.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import de adaptador para 'enzyme-adapter-react-15
+//shallow(): permite testear un componente sin pasar por el DOM
+import {shallow} from 'enzyme';
+import ComponentFunctionsButton from '../../componentsFunctions/componentFunctionsButton';
+
+describe('ComponentFunctionsButton', function() {
+    it('Debería ser un elemento con className="btn"', function() {
+        expect(shallow(<button className="btn"/>).is('.btn')).toBe(true);
+    })  
+});
