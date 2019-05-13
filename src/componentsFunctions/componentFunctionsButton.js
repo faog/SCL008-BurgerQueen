f4bb992c96ac9a7c45ed234bc5717dc12cbdf99e
faog <fabiola.orellana.g@gmail.com>
2019-05-13 11:29:42 -0400

    Configuración de Adapter y enzyme para poder realizar test

--- src/componentsFunctions/componentFunctionsButton.js
+++ src/componentsFunctions/componentFunctionsButton.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import ComponentVisualButton from '../componentsVisual/componentVisualButton';
 import ComponentVisualContainerWaiter from '../componentsVisual/componentVisualContainerWaiter';
 import ComponentVisualContainerKitchen from '../componentsVisual/componentVisualContainerKitchen';
+configure({ adapter: new Adapter() });
 
 class ComponentFunctionsButton extends Component {
 
@@ -17,8 +20,8 @@ class ComponentFunctionsButton extends Component {
     render(){
         return (
             <div>
-                <ComponentVisualButton roles='Mesero' buttonOnClick={this.containerWaiter}/>
-                <ComponentVisualButton roles='Cocina' buttonOnClick={this.containerKitchen}/>
+                <ComponentVisualButton name='Mesero' buttonOnClick={this.containerWaiter}/>
+                <ComponentVisualButton name='Cocina' buttonOnClick={this.containerKitchen}/>
                 <div id="containerrole"></div>  
             </div>
             
