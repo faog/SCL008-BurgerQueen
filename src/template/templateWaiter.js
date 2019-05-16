33482cad3f08216d1ba962a6f58ab38ad28f788e
faog <fabiola.orellana.g@gmail.com>
2019-05-15 22:18:14 -0400

    Creación de constructor, metodo y componente para poder mostrar un mensaje si el mesero elige alguno de los menu (desayuno o resto del dia)

--- src/template/templateWaiter.js
+++ src/template/templateWaiter.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import ComponentVisualButton from '../componentsVisual/componentVisualButton';
+import ComponentVisualMenuOptions from '../componentsVisual/componentVisualMenuOptions';
 
 class TemplateWaiter extends Component {
+  constructor(props) {
+    super(props);
+    this.showMenu = this.showMenu.bind(this);
+  }
+
+  showMenu(menuname) {
+    ReactDOM.render(<ComponentVisualMenuOptions menuname={menuname} />, document.getElementById('menuoptions'));
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm">
             <h5>Ingrese la orden</h5>
-            <ComponentVisualButton name="Desayuno" />
-            <ComponentVisualButton name="Resto del día" />
+            <ComponentVisualButton name="Desayuno" buttonOnClick={e => this.showMenu('Desayuno', e)} />
+            <ComponentVisualButton name="Resto del día" buttonOnClick={e => this.showMenu('Resto del día', e)} />
+            <div id="menuoptions" />
           </div>
           <div className="col-sm">
             <h5>Resumen Pedido</h5>
