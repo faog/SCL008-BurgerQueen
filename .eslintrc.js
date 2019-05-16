e38e43186a9f26a65122ab0cef9f0c7119ff11b2
faog <fabiola.orellana.g@gmail.com>
2019-05-16 14:01:35 -0400

    Se agrego regla a eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   rules: {
     "linebreak-style": 0,
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "react/prefer-stateless-function": [0, { "ignorePureComponents": true }]    
+    "react/prefer-stateless-function": [0, { "ignorePureComponents": true }],
+    "react/destructuring-assignment": [<enabled/>, 'always']    
   },
 };
