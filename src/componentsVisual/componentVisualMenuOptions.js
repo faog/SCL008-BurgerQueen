6422525ba1ac36a266c464691b6757d46eb90052
faog <fabiola.orellana.g@gmail.com>
2019-05-15 23:34:16 -0400

    Se recorre el archivo JSON para renderizar una lista con el menu del desayuno

--- src/componentsVisual/componentVisualMenuOptions.js
+++ src/componentsVisual/componentVisualMenuOptions.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
+const menuFile = require('../data/menuBurgerQueen');
+
 class ComponentVisualMenuOptions extends Component {
+  breakfast() {
+    const breakfastElement = menuFile.breakfast.map(e => {
+      return (
+        <li>{e.name}-{e.price}</li>
+      )
+    })
+    return breakfastElement;
+  }
+
+  lunch() {
+    return JSON.stringify(menuFile.lunch);
+  }
+
   render() {
     return (
-      <div>Menu {this.props.menuname}</div>
+      <div>
+        {this.props.menuname}
+        {this.props.menuname === 'Desayuno' ? this.breakfast() : ''}
+        {this.props.menuname === 'Resto del día' ? this.lunch() : ''}
+      </div>
     );
   }
 }
