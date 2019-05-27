bc1fdc4dd137eee0a3b377708d8a5faaad8929ac
faog <fabiola.orellana.g@gmail.com>
2019-05-27 12:18:10 -0400

    Se agrega los elementos del modal a la comanda del cliente

--- src/componentsVisual/componentVisualModal.js
+++ src/componentsVisual/componentVisualModal.js
@@ -6,17 +6,60 @@ class ComponentVisualModal extends Component {
     super(props, context);
     this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.setBurgerTopping = this.setBurgerTopping.bind(this);
+    this.setBurgerType = this.setBurgerType.bind(this);
     this.state = {
       show: false,
+      product:{}
     };
   }
 
   handleClose() {
-    this.setState({ show: false });
+    this.setState({ 
+      show: false 
+    });
   }
 
   handleShow() {
-    this.setState({ show: true });
+    this.setState({ 
+      show: true,
+      product:{}
+    });
+  }
+
+  handleAdd(){
+    this.setState({
+      show:false
+    });
+    console.log(this.state.product);
+    if(this.state.product.product){
+      this.props.onAddToBill(this.state.product);
+    }
+  }
+
+  setBurgerType(type){
+    this.setState({
+      product:{
+        product:this.props.product,
+        size:this.props.subproduct,
+        topping:this.state.product.topping,
+        type:type,
+        price:this.props.price
+      }
+    });
+  }
+
+  setBurgerTopping(topping,price){
+    this.setState({
+      product:{
+        product:this.props.product,
+        size:this.props.subproduct,
+        topping:topping,
+        type:this.state.product.type,
+        price:this.props.price + price
+      }
+    });
   }
 
   render() {
@@ -31,7 +74,7 @@ class ComponentVisualModal extends Component {
             {this.props.types.map((burgertype)=>{
               return (
                 <ToggleButton type="radio" name="typesradio" className="m-1" variant="primary" value={burgertype} 
-                key={burgertype}>
+                key={burgertype} onClick={evt=>this.setBurgerType(burgertype,evt)}>
                   {burgertype}
                 </ToggleButton>
               )
@@ -42,13 +85,14 @@ class ComponentVisualModal extends Component {
           <div className="row m-1">
             <ToggleButtonGroup className="row mt-3" name="toppingsgroup">
               <ToggleButton type="radio" name="toppingsradio" className="m-1" variant="primary"
-              value="" key="notopping" defaultChecked>
+              value="" key="notopping" defaultChecked onClick={evt=>this.setBurgerTopping("",0,evt)}>
                 Ninguno
               </ToggleButton>
               {this.props.toppings.map((topping)=>{
                 return (
                   <ToggleButton type="radio" name="toppingsradio" className="m-1" variant="primary"
-                  value={topping.product} key={this.props.product+"_"+topping.product}>
+                  value={topping.product} key={this.props.product+"_"+topping.product}
+                  onClick={evt=>this.setBurgerTopping(topping.product,topping.price,evt)}>
                     {topping.product}
                   </ToggleButton>
                 )
@@ -60,7 +104,7 @@ class ComponentVisualModal extends Component {
           <Button variant="secondary" onClick={this.handleClose}>
             Cancelar
           </Button>
-          <Button variant="primary" onClick={this.handleClose}>
+          <Button variant="primary" onClick={this.handleAdd}>
             Agregar
           </Button>
         </Modal.Footer>
