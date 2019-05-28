c590078932b82f2f6752ce2cac835a5a0bb21c8f
faog <fabiola.orellana.g@gmail.com>
2019-05-28 11:05:07 -0400

    Corrección de errores de eslint

--- src/componentsVisual/componentVisualBill.js
+++ src/componentsVisual/componentVisualBill.js
@@ -1,3 +1,7 @@
+/* eslint-disable template-curly-spacing */
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 
@@ -7,30 +11,38 @@ class ComponentVisualBill extends Component {
     this.deleteProduct = this.deleteProduct.bind(this);
   }
 
-  deleteProduct(index){
+  deleteProduct(index) {
     this.props.onDeleteProduct(index);
   }
 
-  render(){
-      return(
-        <div>
-            {this.props.products.map((product, index)=>{
-                return(
-                  <div className="row border" key={index}>
-                    <div className="col-md-11 align-middle"> 
-                      {product.product} {product.size} {product.type} 
-                      {product.topping && product.topping!==""? "con " + product.topping:""}
-                      ${product.price} 
-                      <Button className="col-md-1 align-middle" variant="outline-danger" size="sm" key={"btn"+index}
-                      onClick={evt=>this.deleteProduct(index,evt)}>
-                        X
-                      </Button>
-                    </div>
-                  </div>  
-                )
-            })}
-        </div>
-      )
+  render() {
+    return (
+      <div>
+        {this.props.products.map((product, index) => (
+          <div className="row border" key={index}>
+            <div className="col-md-11 align-middle">
+              {product.product}
+              {' '}
+              {product.size}
+              {' '}
+              {product.type}
+              {product.topping && product.topping !== '' ? ` con ${product.topping}` : ''}
+                      $
+              {product.price}
+              <Button
+                className="col-md-1 align-middle"
+                variant="outline-danger"
+                size="sm"
+                key={`btn ${index}`}
+                onClick={evt => this.deleteProduct(index, evt)}
+              >
+                X
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
