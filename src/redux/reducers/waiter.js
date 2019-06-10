6b3bcff03acaaad5be8d8ace60fd2c1c61aa4db1
faog <fabiola.orellana.g@gmail.com>
2019-06-10 11:09:12 -0400

    Implementación para poder agregar bebestibles al pedido

--- src/redux/reducers/waiter.js
+++ src/redux/reducers/waiter.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { ADD_TO_BILL, DELETE_TO_BILL } from '../actions/actionTypes';
 
 export default (
@@ -8,6 +9,14 @@ export default (
 ) => {
   switch (action.type) {
     case ADD_TO_BILL:
+      //  Convierte a objeto plano (caso de los bebestibles)
+      if (Array.isArray(action.order)) {
+        action.order = {
+          product: action.order[0].product,
+          size: action.order[1].product,
+          price: action.order[1].price,
+        };
+      }
       return {
         ...state,
         orders: state.orders.concat(action.order),
