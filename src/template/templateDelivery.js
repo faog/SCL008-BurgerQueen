a045a7752239a698f85ca822d22ad7c830ef7a0b
faog <fabiola.orellana.g@gmail.com>
2019-06-13 12:07:58 -0400

    estructura básica para poder visualizar el template

---
+++ src/template/templateDelivery.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TemplateDelivery extends Component {
+  render() {
+    return (
+      <h1>Entrega de pedidos</h1>
+    );
+  }
+}
+
+export default TemplateDelivery;
