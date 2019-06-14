1f434a32159902866832eb0f51b60ad20810ecbf
faog <fabiola.orellana.g@gmail.com>
2019-06-13 20:17:11 -0400

    Avance en los test, con errores

--- src/redux/actions/test/action.test.js
+++ src/redux/actions/test/action.test.js
@@ -2,16 +2,39 @@
 import * as actions from '../waiter';
 import * as types from '../actionTypes';
 
-describe('actions', () => {
+describe('action', () => {
+  it('test', () => {
+    const product = {
+      product: 'Cafe',
+      price: 1000,
+    };
+    const expectedAction = {
+      type: types.ADD_TO_BILL,
+      order: product,
+    };
+    const retFunc = actions.addProductToOrder(product);
+    retFunc((receivedAction) => {
+      expect(receivedAction).toEqual(expectedAction);
+    });
+  });/*
+
   it('Crear la acción de agregar un producto ', () => {
-    const product = 'product';
+    const myMock = jest.fn(() => (
+      { product: 'Cafe', type: 'ADD_TO_BILL' }));
+    console.log(myMock());
+    const product = 'Cafe';
     const expectedAction = {
       type: types.ADD_TO_BILL,
       product,
     };
-    expect(actions.addProductToOrder(product)).toEqual(expectedAction);
-  });
+    actions.addProductToOrder(myMock.mock.calls)({
+      type: types.ADD_TO_BILL,
+      product: 'Cafe',
+    });
 
+    expect(myMock.call(expectedAction));
+  }); */
+/*
   it('Crear la acción de eliminar un producto ', () => {
     const index = 'product';
     const expectedAction = {
@@ -29,4 +52,5 @@ describe('actions', () => {
     };
     expect(actions.cleanProductToOrder(product)).toEqual(expectedAction);
   });
+  */
 });
